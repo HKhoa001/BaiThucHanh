69dde2ca7287c20c25df6e0e502f64f5355ee46e
HKHOA001 <hoangtrangtkt@gmail.com>
2023-03-06 22:48:16 +0700

    commit note

--- baithuchanh_2802/src/baithuchanh/Slide55.java
+++ baithuchanh_2802/src/baithuchanh/Slide55.java
@@ -2,7 +2,7 @@ package baithuchanh;
 
 import java.util.Scanner;
 
-public class slide55 {
+public class Slide55 {
     public static void main(String[] args) {
         System.out.println("Cac so chia het cho 5 la: ");
 
