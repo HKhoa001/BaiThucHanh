c568473e673b48053ceb5de8c3a03d7a6898568d
HKHOA001 <hoangtrangtkt@gmail.com>
2023-03-19 11:27:04 +0700

    commit note

--- BaiThucHanhLab2/src/Bai10.java
+++ BaiThucHanhLab2/src/Bai10.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 public class Bai10 {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String a = new String();
-        System.out.println("Nhap chuoi: ");
-        a = sc.nextLine();
-        
-        System.out.printf("Chuoi co %d ky tu\n" ,a.length());
-        
+        String chuoi;
+        char Kytu;
+        int Dem = 0;
+        do{
+            System.out.println("Nhap chuoi: ");
+        chuoi = sc.nextLine();
+        }while (chuoi.length()>80 );
+        System.out.println("Nhap ki tu can tim: ");
+        Kytu = sc.next().charAt(0);
+
+        for (int i = 0; i < chuoi.length(); i++) {
+            if (chuoi.charAt(i) == Kytu) {
+                Dem++;
+            }
+        }
+             
+        System.out.println("So lan xuat hien cua ky tu " + Kytu +
+            " trong chuoi la " + Dem);
     }
 }
--- BaiThucHanhLab2/src/Bai9.java
+++ BaiThucHanhLab2/src/Bai9.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Bai9 {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+        String str;
+        int KTT=0,So=0,KTH=0;
+
         System.out.println("Nhap chuoi: ");
-        String a = sc.nextLine();
-        
-        System.out.printf("Chuoi co %d ky tu\n" ,a.length());
-        
+        str = sc.nextLine();
+
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c>='a'&& c<='z') KTT++;
+            else if(Character.isDigit(c)) So++;
+            else KTH++;                     
+        }
+
+        System.out.print("Co "+KTT+" ky tu thuong, ");
+        System.out.print(KTH+" ky tu hoa ");
+        System.out.print("va "+So+" so.");
     }
 }
