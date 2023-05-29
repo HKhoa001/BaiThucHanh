bb8ccc81a23cf7a3b5cbc4c3570f6a46ee8de2c0
HKHOA001 <hoangtrangtkt@gmail.com>
2023-05-30 06:57:26 +0700

    commit note

--- BaiThucHanhLab4/src/Demo/Student.java
+++ BaiThucHanhLab4/src/Demo/Student.java
@@ -1,8 +1,21 @@
-// package Demo;
+package Demo;
 
-// import java.util.Scanner;
-// import java.util.Map;
+import java.util.Scanner;
+import java.util.Map;
 
-// public class Student {
+public class Student {
+    public static Scanner sc = new Scanner(System.in);
+    public String Msv,ten,lop;
+
+    void nhap(Map<String,Student> Sv,Scanner sc,int n){
+        for(int i = 0;i<n;i++){
+            Student sv = new Student();
+            Msv=sc.nextLine();
+            ten=sc.nextLine();
+            lop=sc.nextLine();
+            Sv.put(sv.Msv,sv);
+        }
+    }
+
     
-// }
+}
--- BaiThucHanhLab4/src/Demo/mainclass.java
+++ BaiThucHanhLab4/src/Demo/mainclass.java
@@ -1,12 +1,16 @@
-// package Demo;
+package Demo;
 
-// import java.util.Scanner;
-// import java.util.Map;
-// import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
-// public class mainclass{
-// public static Scanner sc = new Scanner(System.in);
-//     public static void main (String[] args) {
-//         Map <String,Student> Sv = new HashMap<>();
-//     }
-// }
+public class mainclass{
+public static Scanner sc = new Scanner(System.in);
+    public static void main (String[] args) {
+        Map <String,Student> Sv = new HashMap<>();
+        Student St = new Student();
+        int n;
+        n = sc.nextInt();
+        St.nhap(Sv, sc, n);
+    }
+}
