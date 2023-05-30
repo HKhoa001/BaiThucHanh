0cd9278cc34f16b26a42bda0859e8f3ac75c6c60
HKHOA001 <hoangtrangtkt@gmail.com>
2023-05-30 08:13:45 +0700

    HDK700cau1

--- BaiKiemTraJavaOOP0700/src/App.java
+++ BaiKiemTraJavaOOP0700/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      
+        ArrayList <HDK700Student> List = new ArrayList<>(); 
+    System.out.println("Nhap vao so sinh vien muon tao");
+    int n = sc.nextInt();
+
+    HDK700Student St = new HDK700Student();
+
+    St.Nhap(List,n);
+    St.inThongTin(List);
     }
 }
